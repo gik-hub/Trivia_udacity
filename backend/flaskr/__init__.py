fbd8c961fb26fb8f8a25fb3ec88943aace560983
Titus Thumbi <gikandi95@gmail.com>
2022-08-26 09:54:22 +0300

    add flask CORS and its configuration in the application

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import random
 
 from models import setup_db, Question, Category
@@ -10,16 +10,32 @@ QUESTIONS_PER_PAGE = 10
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     setup_db(app)
-
+    
     """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    @DONE: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
-
+    #   CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    
+    
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    # CORS Headers 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+    
+    # Route specific CORS access
+    @app.route('/messages')
+    @cross_origin()
+    def get_messages():
+        return 'GETTING MESSAGES'
+
 
     """
     @TODO:
